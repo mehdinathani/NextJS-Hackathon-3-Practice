5e2c65aa41b58538ae5b498d749018ae0941b544
mehdinathani <mehdinathani@gmail.com>
2025-01-10 08:45:32 +0500

    bug fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -110,7 +110,7 @@ export default async function ProductsPage() {
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                 {data.map((product, index) => {
                     return (
-                        <Link href={`productDetail/${product.slug}`} >
+                        <Link key={product.slug} href={`productDetail/${product.slug}`} >
                             <ProductCard
                                 key={index}
                                 name={product.title}
